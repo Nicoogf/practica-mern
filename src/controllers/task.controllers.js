1b57f02a1021d0779acec6c5c9f42c6ece322e3d
Nicolas <nicolasgfalabella@gmail.com>
2024-06-10 10:22:20 -0300

    Task Ref

--- src/controllers/task.controllers.js
+++ src/controllers/task.controllers.js
@@ -1,18 +1,40 @@
-export const getTasks = (req,res) => {
+import Task from "../models/task.model.js"
 
+export const getTasks = async (req, res) => {
+    const tasks = await Task.find({
+        user: req.user.id
+    })
+    res.json(tasks)
 }
 
-export const createTask = (req,res) => {
+export const createTask = async (req, res) => {
+    const { title, description, date } = req.body
+    const newTask = new Task({
+        title,
+        description,
+        date,
+        user: req.user.id
+    })
 
+    const savedTask = await newTask.save()
+    res.json(savedTask)
 }
 
-export const getTask = (req,res) => {
-
+export const getTask =  async(req, res) => {
+    const task = await Task.findById(req.params.id)
+    if(!task) return res.statud(404).json({messsage : "tarea no encontrada"})
+    res.json(task)
 }
 
-export const updateTask  = (req,res) => {
-
+export const deleteTask = async(req, res) => {
+    const task = await Task.findByIdAndDelete(req.params.id)
+    if(!task) return res.statud(404).json({messsage: "tarea no encontrada"})
+    res.json(task)
 }
-export const deleteTask = (req,res) => {
 
+
+export const updateTask = async(req, res) => {
+    const task = await Task.findByIdAndUpdate(req.params.id , req.body , {new:true} )
+    if(!task) return res.statud(404).json({messsage: "tarea no encontrada"})
+    res.json(task)
 }
