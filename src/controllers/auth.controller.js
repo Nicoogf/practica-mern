7875c231949d7b9cdf3705773bfad8e269b590f3
Nicolas <nicolasgfalabella@gmail.com>
2024-06-10 09:12:18 -0300

    Logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -30,6 +30,35 @@ export const register = async (req, res) => {
     }
 };
 
-export const loguin = (req, res) => {
-    res.send("loguin")
+export const loguin = async (req, res) => {
+    const { email , password } = req.body
+    try {
+        const UserFound =  await User.findOne({email})
+        if(!UserFound) return res.status(400).json({message : "Usuario no encontrado" })
+
+        const isMatch = await bcrypt.compare(password, UserFound.password)  
+        
+        if(!isMatch) return res.status(400).json({ message : "Credenciales incorrectas, entro por pass"})
+
+       const token = await createAccessToken({ id: UserFound._id })
+
+        res.cookie("token", token)
+      
+        res.json({
+            id: UserFound._id,
+            username : UserFound.username,
+            email: UserFound.email,
+            createdAt: UserFound.createdAt,
+            updateAt : UserFound.updatedAt
+        })
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
 };
+
+export const logout = ( req,res ) => {
+    res.cookie("token" , "" , {
+        expires: new Date(0)
+    })
+    return res.sendStatus(200)
+}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { loguin, register } from "../controllers/auth.controller.js";
+import { logout, loguin, register } from "../controllers/auth.controller.js";
 
 const router = Router() ;
 
 router.post("/register" , register) ;
 router.post("/loguin" , loguin) ;
+router.post ("/logout" , logout) ;
 
 export default router ;
