a9246c9dfcc0ac62a74ad86fda5ba0890fd1f9a8
Nicolas <nicolasgfalabella@gmail.com>
2024-06-10 09:57:21 -0300

    Task Controllers

---
+++ src/controllers/task.controllers.js
@@ -0,0 +1,18 @@
+export const getTasks = (req,res) => {
+
+}
+
+export const createTask = (req,res) => {
+
+}
+
+export const getTask = (req,res) => {
+
+}
+
+export const updateTask  = (req,res) => {
+
+}
+export const deleteTask = (req,res) => {
+
+}
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { authRequire } from "../middlewares/validateToken.js";
+import { createTask, deleteTask, getTask, getTasks, updateTask } from "../controllers/task.controllers.js";
 
 const router = Router()
 
-router.get("/task" , authRequire ,  (req,res) => res.send("tareas"))
+router.get("/tasks", authRequire, getTasks );
+router.get("/tasks/:id", authRequire , getTask)
+router.post("/tasks", authRequire , createTask)
+router.delete("/tasks/:id", authRequire , deleteTask)
+router.put("/tasks", authRequire, updateTask)
 
-export default router ;
+export default router;
