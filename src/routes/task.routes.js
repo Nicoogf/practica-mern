1cf4f554ce221c8cd04b3f5f8dbc1c6d8f7b181f
Nicolas <nicolasgfalabella@gmail.com>
2024-06-10 14:17:02 -0300

    Validaciones

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { authRequire } from "../middlewares/validateToken.js";
 import { createTask, deleteTask, getTask, getTasks, updateTask } from "../controllers/task.controllers.js";
+import { validateSchema } from "../middlewares/validator.middleware.js";
+import { createTaskSchema } from "../schemas/task.schema.js";
 
 const router = Router()
 
-router.get("/tasks", authRequire, getTasks );
-router.get("/tasks/:id", authRequire , getTask)
-router.post("/tasks", authRequire , createTask)
-router.delete("/tasks/:id", authRequire , deleteTask)
+router.get("/tasks", authRequire, getTasks);
+router.get("/tasks/:id", authRequire, getTask)
+router.post("/tasks", authRequire, validateSchema(createTaskSchema), createTask)
+router.delete("/tasks/:id", authRequire, deleteTask)
 router.put("/tasks/:id", authRequire, updateTask)
 
 export default router;
---
+++ src/schemas/task.schema.js
@@ -0,0 +1,12 @@
+import {z} from "zod" ;
+
+export const createTaskSchema = z.object({
+    title: z.string({
+        required_error: "Titulo requerido"
+    }),
+    description: z.string({
+        required_error : "Descripcion requerida"
+    }),
+    date: z.string().datetime().optional()
+})
+
